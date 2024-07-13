3e955bcda4b61ee8c6d0a0904f0097f1f836e4cb
VValMihova <mishomishkatamishunchev666@gmail.com>
2024-07-13 16:24:08 +0300

    index page update

--- invoice-app/src/main/java/bg/softuni/invoice_app/web/HomeController.java
+++ invoice-app/src/main/java/bg/softuni/invoice_app/web/HomeController.java
@@ -1,13 +1,27 @@
 package bg.softuni.invoice_app.web;
 
+import bg.softuni.invoice_app.model.entity.User;
+import bg.softuni.invoice_app.service.user.UserHelperService;
+import bg.softuni.invoice_app.service.user.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
+  private final UserHelperService userHelperService;
+  
+  public HomeController(UserHelperService userHelperService) {
+    this.userHelperService = userHelperService;
+  }
   
   @GetMapping("/")
-  public String index() {
+  public String getHome(Principal principal, Model model) {
+    if (principal != null) {
+      model.addAttribute("companyName", userHelperService.getUser().getCompanyDetails().getCompanyName());
+    }
     return "index";
   }
 }
