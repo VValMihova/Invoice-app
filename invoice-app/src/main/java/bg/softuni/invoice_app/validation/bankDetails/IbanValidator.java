2720c9d1b90f57970093f87b6450ebe75152cf53
VValMihova <mishomishkatamishunchev666@gmail.com>
2024-07-20 15:47:50 +0300

    update iban validator

--- invoice-app/src/main/java/bg/softuni/invoice_app/validation/bankDetails/IbanValidator.java
+++ invoice-app/src/main/java/bg/softuni/invoice_app/validation/bankDetails/IbanValidator.java
@@ -20,17 +20,6 @@ public class IbanValidator implements ConstraintValidator<ValidIban, String> {
     if (!IBAN_PATTERN.matcher(iban).matches() || iban.length() < 15 || iban.length() > 34) {
       return false;
     }
-    
-    String rearrangedIban = iban.substring(4) + iban.substring(0, 4);
-    
-    StringBuilder numericIban = new StringBuilder();
-    for (char character : rearrangedIban.toCharArray()) {
-      int numericValue = Character.getNumericValue(character);
-      if (numericValue < 0 || numericValue > 35) {
-        return false;
-      }
-      numericIban.append(numericValue);
-    }
-    return new java.math.BigInteger(numericIban.toString()).mod(java.math.BigInteger.valueOf(97)).intValue() == 1;
+    return true;
   }
 }
