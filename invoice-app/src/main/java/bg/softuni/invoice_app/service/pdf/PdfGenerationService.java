a4ca5f3ff0a1024917fa9b6a6f2d4ad46e1256c2
VValMihova <mishomishkatamishunchev666@gmail.com>
2024-07-21 16:42:56 +0300

    utf-8 for report

--- invoice-app/src/main/java/bg/softuni/invoice_app/service/pdf/PdfGenerationService.java
+++ invoice-app/src/main/java/bg/softuni/invoice_app/service/pdf/PdfGenerationService.java
@@ -4,14 +4,17 @@ import bg.softuni.invoice_app.exeption.PdfGenerationException;
 import bg.softuni.invoice_app.model.dto.invoice.InvoiceView;
 import bg.softuni.invoice_app.model.dto.sale.SaleReportDto;
 import bg.softuni.invoice_app.service.invoice.InvoiceService;
+import com.lowagie.text.pdf.BaseFont;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.ByteArrayOutputStream;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Service
@@ -46,13 +49,20 @@ public class PdfGenerationService {
   
   public byte[] generateSalesReportPdf(List<SaleReportDto> reportData) {
     Context context = new Context();
+    context.setLocale(LocaleContextHolder.getLocale());
     context.setVariable("reportData", reportData);
     
     String htmlContent = templateEngine.process("report-pdf", context);
+
     
     try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
       ITextRenderer renderer = new ITextRenderer();
       renderer.setDocumentFromString(htmlContent);
+      renderer.getFontResolver()
+          .addFont("C:/Users/YOGA/Downloads/verdana-font-family/verdana-font-family/verdana.ttf",
+              BaseFont.IDENTITY_H,
+              BaseFont.NOT_EMBEDDED
+          );
 //      String fontPath = getClass().getClassLoader().getResource("fonts/DejaVuSans.ttf").toExternalForm();
 //      renderer.getFontResolver().addFont(fontPath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
 //      renderer.getFontResolver().addFont(fontPath, BaseFont.WINANSI, BaseFont.NOT_EMBEDDED);
