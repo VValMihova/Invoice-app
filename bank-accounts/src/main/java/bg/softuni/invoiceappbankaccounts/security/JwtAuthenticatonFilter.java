58b603998f3d6879c0130b9646f7b3758269c070
VValMihova <mishomishkatamishunchev666@gmail.com>
2024-07-25 20:12:46 +0300

    jwt token bank account

---
+++ bank-accounts/src/main/java/bg/softuni/invoiceappbankaccounts/security/JwtAuthenticatonFilter.java
@@ -0,0 +1,32 @@
+package bg.softuni.invoiceappbankaccounts.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticatonFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain filterChain)
+      throws ServletException, IOException {
+
+    final String authHeader = request.getHeader("Authorization");
+    if (authHeader == null ||
+        authHeader.isBlank() ||
+        !authHeader.startsWith("Bearer")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+    String jwt = authHeader.substring(7);
+
+  }
+}
