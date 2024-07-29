1dffcb051fdd0d6dec8d87ade61917715ac877b1
VValMihova <mishomishkatamishunchev666@gmail.com>
2024-07-29 13:43:37 +0300

    set up for integration tests

--- bank-accounts/src/main/resources/data.sql
+++ bank-accounts/src/main/resources/data.sql
@@ -1,3 +1,5 @@
-INSERT INTO `invoice-app-bank-accounts`.bank_accounts (id, bic, currency, iban, user) VALUES (1, 'DEUTDEFF', 'BGN', 'DE89370400440532013000', '91071533-ae2f-4396-8a46-a7cc3e3d86e0');
-INSERT INTO `invoice-app-bank-accounts`.bank_accounts (id, bic, currency, iban, user) VALUES (2, 'DEUTDEFF', 'USD', 'BG80BNBG96611020345678', 'b50fb3f6-45da-4679-b8c7-564fc4bc1a03');
-INSERT INTO `invoice-app-bank-accounts`.bank_accounts (id, bic, currency, iban, user) VALUES (3, 'DEUTDEFF', 'DEE', 'FR1420041010050500013M02606', 'b50fb3f7-32da-4679-8a46-564fc4bc1a08');
+DELETE FROM bank_accounts;
+
+INSERT INTO bank_accounts (id, bic, currency, iban, user) VALUES (1, 'DEUTDEFF', 'BGN', 'DE89370400440532013000', '91071533-ae2f-4396-8a46-a7cc3e3d86e0');
+INSERT INTO bank_accounts (id, bic, currency, iban, user) VALUES (2, 'DEUTDEFF', 'USD', 'BG80BNBG96611020345678', 'b50fb3f6-45da-4679-b8c7-564fc4bc1a03');
+INSERT INTO bank_accounts (id, bic, currency, iban, user) VALUES (3, 'DEUTDEFF', 'DEE', 'FR1420041010050500013M02606', 'b50fb3f7-32da-4679-8a46-564fc4bc1a08');
