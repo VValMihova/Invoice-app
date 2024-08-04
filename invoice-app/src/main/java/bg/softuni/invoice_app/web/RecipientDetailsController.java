7421f984c26eeb7ce44029cc7883c43c135246ee
VValMihova <mishomishkatamishunchev666@gmail.com>
2024-08-04 15:12:09 +0300

    client add sucess message

--- invoice-app/src/main/java/bg/softuni/invoice_app/web/RecipientDetailsController.java
+++ invoice-app/src/main/java/bg/softuni/invoice_app/web/RecipientDetailsController.java
@@ -6,6 +6,7 @@ import bg.softuni.invoice_app.model.dto.recipientDetails.RecipientDetailsView;
 import bg.softuni.invoice_app.model.entity.RecipientDetails;
 import bg.softuni.invoice_app.service.recipientDetails.RecipientDetailsService;
 import jakarta.validation.Valid;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,15 +14,18 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @RequestMapping("/clients")
 public class RecipientDetailsController {
   private final RecipientDetailsService recipientDetailsService;
+  private final MessageSource messageSource;
   
   public RecipientDetailsController(
-      RecipientDetailsService recipientDetailsService) {
+      RecipientDetailsService recipientDetailsService, MessageSource messageSource) {
     this.recipientDetailsService = recipientDetailsService;
+    this.messageSource = messageSource;
   }
   
   @GetMapping
@@ -43,14 +47,18 @@ public class RecipientDetailsController {
   public String addClient(
       @Valid RecipientDetailsAddDto recipientDetails,
       BindingResult bindingResult,
-      RedirectAttributes redirectAttributes) {
+      RedirectAttributes redirectAttributes,
+      Locale locale) {
+    
     if (bindingResult.hasErrors()) {
       redirectAttributes.addFlashAttribute("recipientDetails", recipientDetails);
       redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.recipientDetails", bindingResult);
       return "redirect:/clients/add";
     }
     recipientDetailsService.addRecipientDetails(recipientDetails);
-    return "clients";
+    String successMessage = messageSource.getMessage("success.client.added", null, locale);
+    redirectAttributes.addFlashAttribute("successMessage", successMessage);
+    return "redirect:/clients";
   }
   
   
